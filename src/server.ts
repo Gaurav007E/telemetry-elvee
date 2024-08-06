5259e02ae4445195a68e3d84a3e665700bf10010
Gaurav007E <>
2024-08-06 21:37:26 +0530

    changes

--- src/server.ts
+++ src/server.ts
@@ -12,34 +12,8 @@ const wss = new WebSocketServer({server});
 
 wss.on('connection', (ws: WebSocket) => {
     console.log('A new client connected!');
-
     ws.on('message', (data: WebSocket.RawData, isBinary: boolean) => {
         console.log("rowData", data)
-
-        if (data instanceof ArrayBuffer) console.log("Array Buffer")
-        if (data instanceof Buffer) {
-
-            const buff = Buffer.from(Buffer.from(data).toJSON().data);
-
-            const jsonString = buff.toString();
-
-            console.log(jsonString)
-
-            // const utf16Decoder = new TextDecoder('UTF8')
-            // console.log(utf16Decoder.decode(data))
-            // console.log("Buffer")
-            // const blob = new Blob([data], {type: 'text/plain; charset=utf-8'});
-            //
-            // blob.text().then(text => console.log(text));
-        }
-        if (Array.isArray(data)) {
-            data.forEach(value => {
-                const utf16Decoder = new TextDecoder('UTF8')
-                console.log(utf16Decoder.decode(value))
-            })
-        }
-
-
     });
 
 
